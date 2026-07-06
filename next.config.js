093d3108a126bebb42067f4db15fbd26dc54850c
Dio Arioseno <dio.arioseno@paracorpgroup.com>
2026-07-06 10:41:44 +0700

    rebuild auth files

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'i.scdn.co' },
-      { protocol: 'https', hostname: 'mosaic.scdn.co' },
-    ],
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: { remotePatterns: [{ protocol: 'https', hostname: 'i.scdn.co' }, { protocol: 'https', hostname: 'mosaic.scdn.co' }] },
 }
 
 module.exports = nextConfig
